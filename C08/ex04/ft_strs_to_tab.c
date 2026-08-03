5438945e36a63298c7f1725756c71681783085b8
yaswebdev <yassinerami034@gmail.com>
2026-08-03 23:44:48 +0100

    push exos

--- C08/ex04/ft_strs_to_tab.c
+++ C08/ex04/ft_strs_to_tab.c
@@ -1,7 +1,54 @@
 #include <stdlib.h>
-#include <ft_stock_str.h>
+#include "ft_stock_str.h"
 
-struct s_stock_str *ft_strs_to_tab(int ac, char **av)
+int	ft_strlen(char *str)
 {
+	int	len;
 
+	len = 0;
+	while (str[len])
+		len++;
+	return (len);
+}
+
+char	*ft_strdup(char *src)
+{
+	int		len;
+	int		i;
+	char	*dest;
+
+	len = ft_strlen(src);
+	dest = (char *)malloc(sizeof(char) * (len + 1));
+	if (!dest)
+		return (NULL);
+	i = 0;
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+struct s_stock_str	*ft_strs_to_tab(int ac, char **av)
+{
+	struct s_stock_str	*tab;
+	int					i;
+
+	tab = (struct s_stock_str *)malloc(sizeof(struct s_stock_str) * (ac + 1));
+	if (!tab)
+		return (NULL);
+	i = 0;
+	while (i < ac)
+	{
+		tab[i].size = ft_strlen(av[i]);
+		tab[i].str = av[i];
+		tab[i].copy = ft_strdup(av[i]);
+		if (!tab[i].copy)
+			return (NULL);
+		i++;
+	}
+	tab[i].str = NULL;
+	return (tab);
 }
